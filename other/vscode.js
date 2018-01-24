93198c50275b54bcad6efb8a3826d7ff3529a837
杨攀 <yangpan@baixing.com>
2018-01-24 11:26:57 +0800

    add vscode setting config

---
+++ other/vscode.js
@@ -0,0 +1,23 @@
+{
+    "javascript.implicitProjectConfig.experimentalDecorators": true,
+    "editor.lineHeight":0,
+    "editor.fontSize": 16,
+    "workbench.colorTheme": "Atom One Dark",
+    "workbench.iconTheme": "vscode-great-icons",
+    "extensions.ignoreRecommendations": false,
+    "background.enabled": true,
+    "background.useDefault": false,
+    "background.customImages": [
+        "/Users/baixing/Desktop/Scarlett-Johansson-hot-wallpaper.jpg"
+    ],
+    "background.style": {
+        "opacity": 0.1,
+        "z-index": "9999",
+        "background-position": "center center",
+    },
+    "python.linting.flake8Enabled": true,
+    "python.disablePromptForFeatures": [
+        "flake8",
+        "pylint"
+    ],
+}
